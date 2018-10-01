389232c97de646c60146f55a035d454844746697
Tobias Richter <tr4711@gmail.com>
2018-10-01 17:19:50 +0200

    Move constant maxtimes from e2e-harness

---
+++ max_times.go
@@ -0,0 +1,12 @@
+package backoff
+
+const (
+	LongMaxWait   = 40 * time.Minute
+	MediumMaxWait = 10 * time.Minute
+	ShortMaxWait  = 2 * time.Minute
+)
+
+const (
+	LongMaxInterval  = 60 * time.Second
+	ShortMaxInterval = 5 * time.Second
+)
