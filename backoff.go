078b7a4105c8cb07469872f492ccc1399cb92862
Paweł Kopiczko <kopiczko@gmail.com>
2018-10-30 09:58:48 +0000

    pawel learns alphabet

--- backoff.go
+++ backoff.go
@@ -7,12 +7,12 @@ import (
 const (
 	LongMaxWait   = 40 * time.Minute
 	MediumMaxWait = 10 * time.Minute
-	TinyMaxWait   = 5 * time.Second
 	ShortMaxWait  = 2 * time.Minute
+	TinyMaxWait   = 5 * time.Second
 )
 
 const (
 	LongMaxInterval  = 60 * time.Second
-	TinyMaxInterval  = 1 * time.Second
 	ShortMaxInterval = 5 * time.Second
+	TinyMaxInterval  = 1 * time.Second
 )
