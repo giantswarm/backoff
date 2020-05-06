a6724b02e7c6208a7f293bd610eb7f61d2a990fd
wtty-fool <wtty.fool@gmail.com>
2020-05-06 09:55:41 +0200

    add tests for custom types

--- matching_test.go
+++ matching_test.go
@@ -5,25 +5,48 @@ import (
 	"testing"
 )
 
-func Test_ErrorMatching(t *testing.T) {
-	customError = errors.New("custom error")
-	differentError = errors.New("different error")
+type simpleTestError struct{}
 
-	tests = map[error]error{
+func (c *simpleTestError) Error() string {
+	return "simpleTestError"
+}
+
+type wrapTestError struct {
+	err error
+}
+
+func (c *wrapTestError) Error() string {
+	return c.err.Error()
+}
+
+func (c *wrapTestError) Unwrap() error {
+	return c.err
+}
+
+func Test_ErrorMatching(t *testing.T) {
+	customError := errors.New("custom error")
+	differentError := errors.New("different error")
+	customTypeError := &simpleTestError{}
+	wrappedError := &wrapTestError{customTypeError}
+
+	tests := map[error]error{
 		// sanity check
 		customError:    customError,
 		differentError: differentError,
 		// actual test
-		Permanent(customError):    customError,
-		Permanent(differentError): differentError,
+		Permanent(customError):     customError,
+		Permanent(differentError):  differentError,
+		Permanent(customTypeError): customTypeError,
+		Permanent(wrappedError):    wrappedError,
+		Permanent(wrappedError):    customTypeError,
 	}
 
-	for err, expectedType := range tests {
-		if !errors.Is(err, expectedType) {
-			t.Fatalf("errors.Is: expected %v to match %T", err, expectedType)
+	for err, expectedTarget := range tests {
+		if !errors.Is(err, expectedTarget) {
+			t.Fatalf("errors.Is: expected %v to match %T", err, expectedTarget)
 		}
-		if !errors.As(err, &expectedType) {
-			t.Fatalf("errors.As: expected %v to match %T", err, expectedType)
+		if !errors.As(err, &expectedTarget) {
+			t.Fatalf("errors.As: expected %v to match %T", err, expectedTarget)
 		}
 	}
 }
